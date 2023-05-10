1e5c0eb5c0e409881fe436fd16996b5e64af94fd
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2023-05-10 19:36:27 +0100

    tweak modal

--- ui/components/app/modals/confirm-clear-contact-list/confirm-clear-contact-list.component.js
+++ ui/components/app/modals/confirm-clear-contact-list/confirm-clear-contact-list.component.js
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {
+  DISPLAY,
+  FLEX_DIRECTION,
   FontWeight,
+  JustifyContent,
+  TextAlign,
   TextVariant,
 } from '../../../../helpers/constants/design-system';
 import withModalProps from '../../../../helpers/higher-order-components/with-modal-props';
 import { Text } from '../../../component-library';
+import Box from '../../../ui/box/box';
 import Modal from '../../modal';
 
 const ConfirmClearContactList = ({ clearContactList, hideModal }) => {
+  const bodyText =
+    'All your contact list will be deleted. You may want to cancel and backup your contact list first.';
   return (
     <Modal
       onSubmit={() => {
@@ -18,12 +25,32 @@ const ConfirmClearContactList = ({ clearContactList, hideModal }) => {
       submitText="Clear contact list"
       onCancel={() => hideModal()}
       cancelText="Cancel"
-      contentClass="customize-nonce-modal-content"
-      containerClass="customize-nonce-modal-container"
     >
-      <Text variant={TextVariant.bodySm} as="h6" fontWeight={FontWeight.Normal}>
-        Very cool explanatory text
-      </Text>
+      <Box
+        display={DISPLAY.FLEX}
+        justifyContent={JustifyContent.center}
+        flexDirection={FLEX_DIRECTION.COLUMN}
+      >
+        <Box as="header">
+          <Text
+            as="h1"
+            variant={TextVariant.headingMd}
+            fontWeight={FontWeight.Bold}
+            textAlign={TextAlign.Center}
+          >
+            Are you sure you want to delete your contact list?
+          </Text>
+        </Box>
+        <Box marginTop={4}>
+          <Text
+            as="h3"
+            variant={TextVariant.bodyMd}
+            fontWeight={FontWeight.Normal}
+          >
+            {bodyText}
+          </Text>
+        </Box>
+      </Box>
     </Modal>
   );
 };
