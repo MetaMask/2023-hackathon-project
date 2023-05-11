e862903d31b14822ac024c9db6384379e87ce63f
Amélie Chan <amelie.chan@consensys.net>
2023-05-11 10:12:58 +0200

    Implement add to address book UI notification page

--- ui/pages/add-to-address-book/add-to-address-book.js
+++ ui/pages/add-to-address-book/add-to-address-book.js
@@ -1,188 +1,210 @@
-import React, { useCallback, useContext, useEffect, useMemo } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useContext, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import ActionableMessage from '../../components/ui/actionable-message/actionable-message';
-import Button from '../../components/ui/button';
+import PropTypes from 'prop-types';
+import { isValidHexAddress } from '@metamask/controller-utils';
+import { getMostRecentOverviewPage } from '../../ducks/history/history';
 import { PageContainerFooter } from '../../components/ui/page-container';
 import { I18nContext } from '../../contexts/i18n';
-import { MetaMetricsContext } from '../../contexts/metametrics';
-import { getMostRecentOverviewPage } from '../../ducks/history/history';
-import { getTokens } from '../../ducks/metamask/metamask';
-import ZENDESK_URLS from '../../helpers/constants/zendesk-url';
-import { isEqualCaseInsensitive } from '../../../shared/modules/string-utils';
-import { getSuggestedAssets } from '../../selectors';
-import { rejectWatchAsset, acceptWatchAsset } from '../../store/actions';
+import TextField from '../../components/ui/text-field';
+import Box from '../../components/ui/box/box';
+import CheckBox from '../../components/ui/check-box';
+import { Label, Text } from '../../components/component-library';
 import {
-  MetaMetricsEventCategory,
-  MetaMetricsEventName,
-  MetaMetricsTokenEventSource,
-} from '../../../shared/constants/metametrics';
-import {
-  AssetType,
-  TokenStandard,
-} from '../../../shared/constants/transaction';
+  AlignItems,
+  FLEX_DIRECTION,
+  TextColor,
+  TextVariant,
+} from '../../helpers/constants/design-system';
+import { isValidDomainName } from '../../helpers/utils/util';
+import { isBurnAddress } from '../../../shared/modules/hexstring-utils';
+import { INVALID_RECIPIENT_ADDRESS_ERROR } from '../send/send.constants';
 
-/**
- * @param {Array} suggestedAssets - an array of assets suggested to add to the user's wallet
- * via the RPC method `wallet_watchAsset`
- * @param {Array} tokens - the list of tokens currently tracked in state
- * @returns {boolean} Returns true when the list of suggestedAssets contains an entry with
- *          an address that matches an existing token.
- */
-function hasDuplicateAddress(suggestedAssets, tokens) {
-  const duplicate = suggestedAssets.find(({ asset }) => {
-    const dupe = tokens.find(({ address }) => {
-      return isEqualCaseInsensitive(address, asset.address);
-    });
-    return Boolean(dupe);
-  });
-  return Boolean(duplicate);
-}
+const ALLOW_LIST = 'allowList';
+const BLOCK_LIST = 'blockList';
 
-/**
- * @param {Array} suggestedAssets - a list of assets suggested to add to the user's wallet
- * via RPC method `wallet_watchAsset`
- * @param {Array} tokens - the list of tokens currently tracked in state
- * @returns {boolean} Returns true when the list of suggestedAssets contains an entry with both
- *          1. a symbol that matches an existing token
- *          2. an address that does not match an existing token
- */
-function hasDuplicateSymbolAndDiffAddress(suggestedAssets, tokens) {
-  const duplicate = suggestedAssets.find(({ asset }) => {
-    const dupe = tokens.find((token) => {
-      return (
-        isEqualCaseInsensitive(token.symbol, asset.symbol) &&
-        !isEqualCaseInsensitive(token.address, asset.address)
-      );
-    });
-    return Boolean(dupe);
-  });
-  return Boolean(duplicate);
-}
-
-const AddToAddressBook = () => {
-  console.log('in address book');
+const AddToAddressBook = ({
+  address = '',
+  name = '',
+  memo = '',
+  addToAddressBook,
+}) => {
   const t = useContext(I18nContext);
-  const dispatch = useDispatch();
+  const mostRecentOverviewPage = useSelector(getMostRecentOverviewPage);
   const history = useHistory();
 
-  const mostRecentOverviewPage = useSelector(getMostRecentOverviewPage);
-  const suggestedAssets = useSelector(getSuggestedAssets);
-  const tokens = useSelector(getTokens);
+  const [nameInput, setNameInput] = useState(name === null ? '' : name);
+  const [memoInput, setMemoInput] = useState(memo === null ? '' : memo);
+  const [addressInput, setAddressInput] = useState(
+    address === null ? '' : address,
+  );
+  const [tags, setTags] = useState([]);
+  const [error, setError] = useState('');
 
-  const trackEvent = useContext(MetaMetricsContext);
-
-  const knownTokenActionableMessage = useMemo(() => {
-    return (
-      hasDuplicateAddress(suggestedAssets, tokens) && (
-        <ActionableMessage
-          message={t('knownTokenWarning', [
-            <Button
-              type="link"
-              key="confirm-add-suggested-token-duplicate-warning"
-              className="confirm-add-suggested-token__link"
-              rel="noopener noreferrer"
-              target="_blank"
-              href={ZENDESK_URLS.TOKEN_SAFETY_PRACTICES}
-            >
-              {t('learnScamRisk')}
-            </Button>,
-          ])}
-          type="warning"
-          withRightButton
-          useIcon
-          iconFillColor="#f8c000"
-        />
-      )
-    );
-  }, [suggestedAssets, tokens, t]);
-
-  const reusedTokenNameActionableMessage = useMemo(() => {
-    return (
-      hasDuplicateSymbolAndDiffAddress(suggestedAssets, tokens) && (
-        <ActionableMessage
-          message={t('reusedTokenNameWarning')}
-          type="warning"
-          withRightButton
-          useIcon
-          iconFillColor="#f8c000"
-        />
-      )
-    );
-  }, [suggestedAssets, tokens, t]);
-
-  const handleAddTokensClick = useCallback(async () => {
-    await Promise.all(
-      suggestedAssets.map(async ({ asset, id }) => {
-        await dispatch(acceptWatchAsset(id));
-
-        trackEvent({
-          event: MetaMetricsEventName.TokenAdded,
-          category: MetaMetricsEventCategory.Wallet,
-          sensitiveProperties: {
-            token_symbol: asset.symbol,
-            token_contract_address: asset.address,
-            token_decimal_precision: asset.decimals,
-            unlisted: asset.unlisted,
-            source_connection_method: MetaMetricsTokenEventSource.Dapp,
-            token_standard: TokenStandard.ERC20,
-            asset_type: AssetType.token,
-          },
-        });
-      }),
-    );
-
-    history.push(mostRecentOverviewPage);
-  }, [dispatch, history, trackEvent, mostRecentOverviewPage, suggestedAssets]);
-
-  const goBackIfNoSuggestedAssetsOnFirstRender = () => {
-    if (!suggestedAssets.length) {
-      history.push(mostRecentOverviewPage);
+  const validate = (newAddress) => {
+    const valid =
+      !isBurnAddress(newAddress) &&
+      isValidHexAddress(newAddress, { mixedCaseUseChecksum: true });
+    const validEnsAddress = isValidDomainName(newAddress);
+    if (valid || validEnsAddress || newAddress === '') {
+      setError('');
+    } else {
+      setError(INVALID_RECIPIENT_ADDRESS_ERROR);
     }
   };
 
-  useEffect(() => {
-    goBackIfNoSuggestedAssetsOnFirstRender();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const handleNameChange = (event) => {
+    setNameInput(event.target.value);
+    validate(event.target.value);
+  };
+
+  const handleMemoChange = (event) => {
+    setMemoInput(event.target.value);
+  };
+
+  const handleAddressChange = (event) => {
+    setAddressInput(event.target.value);
+    validate(event.target.value);
+  };
+
+  const addAllowList = () => {
+    if (tags.includes(ALLOW_LIST)) {
+      setTags(tags.filter((tag) => tag !== ALLOW_LIST));
+    } else {
+      setTags([...tags, ALLOW_LIST]);
+    }
+  };
+
+  const addBlockList = () => {
+    if (tags.includes(BLOCK_LIST)) {
+      setTags(tags.filter((tag) => tag !== BLOCK_LIST));
+    } else {
+      setTags([...tags, BLOCK_LIST]);
+    }
+  };
 
   return (
     <div className="page-container">
       <div className="page-container__header">
-        <div className="page-container__title">{t('addSuggestedTokens')}</div>
+        <div className="page-container__title">Add contact</div>
         <div className="page-container__subtitle">
-          {t('likeToImportTokens')}
+          Add a new contact to your address book and choose whether they should
+          be on your allow or block list.
         </div>
-        {knownTokenActionableMessage}
-        {reusedTokenNameActionableMessage}
       </div>
       <div className="page-container__content">
-        <div className="confirm-add-suggested-token">
-          <div className="confirm-add-suggested-token__header">
-            <div className="confirm-add-suggested-token__token">
-              {t('token')}
-            </div>
-            <div className="confirm-add-suggested-token__balance">
-              {t('balance')}
-            </div>
+        <div className="add-to-address-book__content">
+          <div className="add-to-address-book__content__name-label">
+            {t('address')}
           </div>
-          <div className="confirm-add-suggested-token__token-list">hello</div>
+          <TextField
+            className="add-to-address-book__content__text-field"
+            value={addressInput}
+            onChange={handleAddressChange}
+            placeholder="Add an address"
+            fullWidth
+          />
+          {error && (
+            <div className="address-book__add-contact__error">{t(error)}</div>
+          )}
+          <div className="add-to-address-book__content__name-label">
+            {t('name')}
+          </div>
+          <TextField
+            className="add-to-address-book__content__text-field"
+            value={nameInput}
+            onChange={handleNameChange}
+            placeholder="Add a name"
+            fullWidth
+          />
+          <div className="add-to-address-book__content__label--capitalized">
+            {t('memo')}
+          </div>
+          <TextField
+            type="text"
+            id="memo"
+            value={memoInput}
+            onChange={handleMemoChange}
+            placeholder={t('addMemo')}
+            fullWidth
+            margin="dense"
+            multiline
+            rows={3}
+            classes={{
+              inputMultiline: 'add-to-address-book__content__text-area',
+              inputRoot: 'add-to-address-book__content__text-area-wrapper',
+            }}
+          />
+        </div>
+        <div className="add-to-address-book__row">
+          <Box
+            flexDirection={FLEX_DIRECTION.ROW}
+            alignItems={AlignItems.flexStart}
+            paddingLeft={4}
+            paddingBottom={4}
+            gap={2}
+          >
+            <CheckBox
+              value="allowList"
+              onClick={addAllowList}
+              id="allow-list-checkbox"
+              checked={tags.length > 0 && tags.includes(ALLOW_LIST)}
+            />
+            <Label htmlFor="allow-list-checkbox">
+              <Text
+                variant={TextVariant.bodyMdBold}
+                as="span"
+                color={TextColor.successDefault}
+              >
+                Allow List
+              </Text>
+            </Label>
+          </Box>
+          <Box
+            flexDirection={FLEX_DIRECTION.ROW}
+            alignItems={AlignItems.flexStart}
+            paddingLeft={4}
+            paddingBottom={4}
+            gap={2}
+          >
+            <CheckBox
+              id="block-list-checkbox"
+              value="blockList"
+              onClick={addBlockList}
+              checked={tags.length > 0 && tags.includes(BLOCK_LIST)}
+            />
+            <Label htmlFor="block-list-checkbox">
+              <Text
+                variant={TextVariant.bodyMdBold}
+                as="span"
+                color={TextColor.errorDefault}
+              >
+                Block List
+              </Text>
+            </Label>
+          </Box>
         </div>
       </div>
       <PageContainerFooter
         cancelText={t('cancel')}
-        submitText={t('addToken')}
+        submitText={t('save')}
         onCancel={async () => {
-          await Promise.all(
-            suggestedAssets.map(({ id }) => dispatch(rejectWatchAsset(id))),
-          );
           history.push(mostRecentOverviewPage);
         }}
-        onSubmit={handleAddTokensClick}
-        disabled={suggestedAssets.length === 0}
+        onSubmit={async () => {
+          await addToAddressBook(addressInput, nameInput, memoInput, tags);
+        }}
+        disabled={Boolean(error || !addressInput || !nameInput.trim())}
       />
     </div>
   );
 };
 
 export default AddToAddressBook;
+
+AddToAddressBook.propTypes = {
+  name: PropTypes.string,
+  address: PropTypes.string,
+  memo: PropTypes.string,
+  addToAddressBook: PropTypes.func,
+};
